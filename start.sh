038072a70a54f41ba378e85519bd01c09d0fb054
TrongThao Hub <nttgameryb@gmail.com>
2025-11-09 02:56:57 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -13,5 +13,7 @@ echo "Starting Flask on port 10000..."
 gunicorn --bind 0.0.0.0:10000 app:app &
 
 echo "Starting Ngrok TCP tunnel (SSH port 22)..."
+echo "Using authtoken from env: $NGROK_AUTHTOKEN"
+ngrok authtoken $NGROK_AUTHTOKEN
 echo "Ngrok URL will appear below. Check logs to get SSH URL."
 ngrok tcp 22 --log=stdout
